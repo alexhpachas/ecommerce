f2c5da7d1a13d798b3e6488825b2cb9d1b02846f
Alex <alex.h.pachas@gmail.com>
2021-08-30 12:26:15 -0500

    responsivo2.9

--- resources/views/livewire/admin/city-component.blade.php
+++ resources/views/livewire/admin/city-component.blade.php
@@ -21,7 +21,7 @@
                         <tr>
                             <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-full">
-                                Nombre de la marca
+                                Nombre de la provincia
                             </th>
                             <th scope="col" class="relative px-6 py-3">
                                 <span class="sr-only">Editar</span>
--- resources/views/livewire/admin/show-department.blade.php
+++ resources/views/livewire/admin/show-department.blade.php
@@ -22,7 +22,7 @@
                         <tr>
                             <th scope="col"
                                 class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-full">
-                                Nombre de la cuidad
+                                Nombre de la provincia
                             </th>
                             <th scope="col" class="relative px-6 py-3">
                                 <span class="sr-only">Editar</span>
