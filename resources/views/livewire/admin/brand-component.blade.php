c9fa3ed58fb9fa007f63b565ee4f8d18d54437e1
Alex <alex.h.pachas@gmail.com>
2021-08-30 11:16:23 -0500

    responsivo2.2

--- resources/views/livewire/admin/brand-component.blade.php
+++ resources/views/livewire/admin/brand-component.blade.php
@@ -3,9 +3,8 @@
     <div class="container">    
 
         <div class="container flex items-center mb-3  bg-white py-4 rounded-lg shadow-lg">
-            <h2 class="font-semibold text-xl text-gray-600 sm:text-xs">
-                LISTA DE MARCAS
-            </h2>
+
+            <span class="font-semibold lg:text-xl text-gray-600 sm:text-sm">LISTA DE MARCAS</span>            
 
             @can('admin.brands.create')                            
                 <x-jet-button class="ml-auto rounded-full transform sm:text-xs hover:scale-105" wire:click="$set('openMarcaCreate',true)">
