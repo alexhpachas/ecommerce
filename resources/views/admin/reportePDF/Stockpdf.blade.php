2467e34d927a4df1b97301b0b9353e2f00fe5a8b
Alex <alex.h.pachas@gmail.com>
2021-09-05 13:01:03 -0500

    correcion 2.3 pdf

--- resources/views/admin/reportePDF/Stockpdf.blade.php
+++ resources/views/admin/reportePDF/Stockpdf.blade.php
@@ -330,7 +330,6 @@
                                     @else
                                         <span class="text-green-600 font-bold">SI</span>
                                     @endif
-                                    
                                 </div>
                             </td>
     
@@ -351,7 +350,16 @@
                                     {{$producto->brand->name}}
                                 </div>                                       
                             </td>
-                            
+                            <td class="py-1 whitespace-nowrap uppercase">
+                                <div class="text-sm">
+                                
+                                </div>                                       
+                            </td>
+                            <td class="py-1 whitespace-nowrap uppercase">
+                                <div class="text-sm">                                        
+                                    
+                                </div>
+                            </td>
     
                             <td class="py-1 whitespace-nowrap uppercase">
                                 <div class="text-sm">
