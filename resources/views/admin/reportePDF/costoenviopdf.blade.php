e6cdaf21d98c1a08f803dcb5fca3aad0debbb921
Alex <alex.h.pachas@gmail.com>
2021-09-05 11:46:53 -0500

    correcion 2.4 pdf

--- resources/views/admin/reportePDF/costoenviopdf.blade.php
+++ resources/views/admin/reportePDF/costoenviopdf.blade.php
@@ -40,59 +40,15 @@
                     
                 </style>
 
-                <tr class="cabecera">
-                    <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        CODIGO
-                    </th>
-                    <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        NOMBRE DEL DEPARTAMENTO
-                    </th>
-
-                    <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        CIUDAD
-                    </th>
-
-                    <th scope="col"
-                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        COSTO DE ENVÍO
-                    </th>                    
-                </tr>
+                
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
                 @foreach ($ciudades as $ciudade)
                         <tr class="cuerpo hover:bg-gray-100 hover:text-red-500 cursor-pointer">
                             <td class="px-6 py-2 whitespace-nowrap">
-                                <div class="flex items-center">
-
-                                    <div class="ml-4">
-                                        <div class="text-sm font-medium ">
-                                            {{ $ciudade->id }}
-                                        </div>
-
-                                    </div>
-                                </div>
+                                {{$cuidade->id}}
                             </td>                            
-                            <td class="px-6 py-2 whitespace-nowrap uppercase">
-                                <div class="text-sm ">
-                                   {{--  @php
-                                        $departamento = \App\Models\Department::find($ciudade->department_id);
-                                    @endphp
-                                     {{$departamento->name}} --}}
-                                </div>
-                            </td>
-
-                            <td class="px-6 py-2 whitespace-nowrap uppercase">
-                                <div class="text-sm ">
-                                    {{$ciudade->name}}
-                                </div>
-                            </td>
-
-                            <td class="px-6 py-2 whitespace-nowrap uppercase">
-                                <div class="text-sm font-bold text-red-600">S/. {{ $ciudade->cost }}</div>
-                            </td>
+                            
         
                         </tr>
                     @endforeach
