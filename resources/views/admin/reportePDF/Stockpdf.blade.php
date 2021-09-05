af75880e76a6ae6bfc1014ce1b7124b1336385de
Alex <alex.h.pachas@gmail.com>
2021-09-05 13:02:22 -0500

    correcion 2.3 pdf

--- resources/views/admin/reportePDF/Stockpdf.blade.php
+++ resources/views/admin/reportePDF/Stockpdf.blade.php
@@ -352,12 +352,12 @@
                             </td>
                             <td class="py-1 whitespace-nowrap uppercase">
                                 <div class="text-sm">
-                                
+                                    <span>A</span>
                                 </div>                                       
                             </td>
                             <td class="py-1 whitespace-nowrap uppercase">
                                 <div class="text-sm">                                        
-                                    
+                                    <span>B</span>
                                 </div>
                             </td>
     
