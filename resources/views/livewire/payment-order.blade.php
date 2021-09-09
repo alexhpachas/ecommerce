9c59ba605a184163ebae9bd4238b0890f8012362
alexhpachas <81926846+alexhpachas@users.noreply.github.com>
2021-09-09 00:24:34 -0500

    script pasarela stripe

--- resources/views/livewire/payment-order.blade.php
+++ resources/views/livewire/payment-order.blade.php
@@ -345,65 +345,67 @@
     </div>
 
     @slot('js')
-        <script>
-            document.addEventListener('livewire:load', function() {
-                stripe();
-            });
-
-            Livewire.on('resetStripe', function() {
-                document.getElementById('card-form').reset();
-                stripe();
-
-
-                /* alert("la compra se realizo con exito"); */
-            });
-
-            Livewire.on('errorPayment', function() {
-                document.getElementById('card-form').reset();
-                stripe();
-                alert("Hubo un Error en la compra intentelo de nuevo");
-            });
-        </script>
-
-        <script>
-            function stripe() {
-                const stripe = Stripe('{{ env('STRIPE_KEY') }}');
-
-                const elements = stripe.elements();
-                const cardElement = elements.create('card');
-
-                cardElement.mount('#card-element');
-
-
-                const cardHolderName = document.getElementById('card-holder-name');
-                const cardButton = document.getElementById('card-button');
-                const cardForm = document.getElementById('card-form');
-
-                cardForm.addEventListener('submit', async (e) => {
-                    e.preventDefault();
-                    const {
-                        paymentMethod,
-                        error
-                    } = await stripe.createPaymentMethod(
-                        'card', cardElement, {
-                            billing_details: {
-                                name: cardHolderName.value
-                            }
-                        }
-                    );
-
-                    if (error) {
-                        document.getElementById('card-error').textContent = error.message;
-                    } else {
-                        Livewire.emit('paymentMethodCreate', paymentMethod.id);
-                    }
-                });
-            }
-        </script>
+        
 
     @endslot
 
     @push('script')
+
+    <script>
+        document.addEventListener('livewire:load', function() {
+            stripe();
+        });
+
+        Livewire.on('resetStripe', function() {
+            document.getElementById('card-form').reset();
+            stripe();
+
+
+            /* alert("la compra se realizo con exito"); */
+        });
+
+        Livewire.on('errorPayment', function() {
+            document.getElementById('card-form').reset();
+            stripe();
+            alert("Hubo un Error en la compra intentelo de nuevo");
+        });
+    </script>
+
+    <script>
+        function stripe() {
+            const stripe = Stripe('{{ env('STRIPE_KEY') }}');
+
+            const elements = stripe.elements();
+            const cardElement = elements.create('card');
+
+            cardElement.mount('#card-element');
+
+
+            const cardHolderName = document.getElementById('card-holder-name');
+            const cardButton = document.getElementById('card-button');
+            const cardForm = document.getElementById('card-form');
+
+            cardForm.addEventListener('submit', async (e) => {
+                e.preventDefault();
+                const {
+                    paymentMethod,
+                    error
+                } = await stripe.createPaymentMethod(
+                    'card', cardElement, {
+                        billing_details: {
+                            name: cardHolderName.value
+                        }
+                    }
+                );
+
+                if (error) {
+                    document.getElementById('card-error').textContent = error.message;
+                } else {
+                    Livewire.emit('paymentMethodCreate', paymentMethod.id);
+                }
+            });
+        }
+    </script>
         {{-- CDN PARA MERCADO PAGO --}}
 
         <script src="https://sdk.mercadopago.com/js/v2"></script>
