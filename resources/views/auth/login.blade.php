63774bb75d2e876e75bfb62f0183e63968d72568
alexhpachas <81926846+alexhpachas@users.noreply.github.com>
2021-09-19 17:40:37 -0500

    Estilos a los boton login Fb y google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -87,18 +87,20 @@
 
                    
 
-                    <div class="flex mt-7 justify-center w-full">
-                        <a href="{{route('login.facebook','facebook')}}"
-                            class="mr-5 bg-blue-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
-
-                            Facebook
+                    <div class="flex mt-7 justify-center w-full gap-4">
+                        <a href="{{route('login.facebook','facebook')}}" class="bg-blue-500 px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                            <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
+                            <span>Facebook</span>
                         </a>
-                        
-                        <a href="{{route('login.facebook','google')}}"
-                            class="bg-red-500 border-none px-4 py-2 rounded-xl cursor-pointer text-white shadow-xl hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
 
-                            Google
+                        <a href="{{route('login.facebook','google')}}" class="bg-red-500 px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded hover:shadow-inner transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"">
+                            <svg class="w-9 h-5 " xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                            viewBox="0 0 172 172"
+                            style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M89.42656,165.12c-43.63156,0 -79.13344,-35.48844 -79.13344,-79.12c0,-43.63156 35.50187,-79.12 79.13344,-79.12c19.76656,0 38.68656,7.32344 53.29312,20.62656l2.66063,2.43219l-26.09563,26.09563l-2.41875,-2.06938c-7.65937,-6.5575 -17.40156,-10.17219 -27.43937,-10.17219c-23.27375,0 -42.22063,18.93344 -42.22063,42.20719c0,23.27375 18.94688,42.20719 42.22063,42.20719c16.78344,0 30.04625,-8.57312 36.29469,-23.17969h-39.73469v-35.62281l77.57469,0.1075l0.57781,2.72781c4.04469,19.20219 0.80625,47.44781 -15.5875,67.65781c-13.57188,16.72969 -33.45938,25.22219 -59.125,25.22219z"></path></g></g>
+                            </svg>
+                            <span>Google</span>
                         </a>
+                                                                      
                     </div>
 
                     <div class="mt-7">
